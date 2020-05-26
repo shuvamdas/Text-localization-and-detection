5c93f3985822158c8277c2aa6aec039433c48b86
shuvamdas <dasshuvam18@gmail.com>
2020-05-27 02:51:38 +0530

    first commit

---
+++ text_tesseract.py
@@ -0,0 +1,46 @@
+# python localize_text_tesseract.py --image apple_support.png
+# python localize_text_tesseract.py --image apple_support.png --min-conf 50
+
+from pytesseract import Output
+import pytesseract
+import argparse
+import cv2
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image to be OCR'd")
+ap.add_argument("-c", "--min-conf", type=int, default=0,
+	help="mininum confidence value to filter weak text detection")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+results = pytesseract.image_to_data(rgb, output_type=Output.DICT)
+
+# loop over each of the individual text localizations
+for i in range(0, len(results["text"])):
+	# extract the bounding box coordinates of the text region from the current result
+	x = results["left"][i]
+	y = results["top"][i]
+	w = results["width"][i]
+	h = results["height"][i]
+
+	# extract the OCR text itself along with the confidence of the text localization
+	text = results["text"][i]
+	conf = int(results["conf"][i])
+
+	# filter out weak confidence text localizations
+	if conf > args["min_conf"]:
+		print("Confidence: {}".format(conf))
+		print("Text: {}".format(text))
+		print("")
+
+		text = "".join([c if ord(c) < 128 else "" for c in text]).strip()
+		cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+			1.2, (0, 0, 255), 3)
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
